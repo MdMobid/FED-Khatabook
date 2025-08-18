78beea83c5fd2a1ba1ad3a08767638ff63a8b264
MdMobid <mobid9021@outlook.com>
2025-08-18 16:33:45 +0530

    + config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+SMTP_SERVER = 'smtp.yourmail.com'
+SMTP_PORT = 587
+SMTP_USERNAME = 'your_email@example.com'
+SMTP_PASSWORD = 'your_email_password'
+
+CREDIT_DUE_DAYS = 30
+BAD_DEBT_THRESHOLD_DAYS = 60
+
+REMINDER_DAYS_BEFORE = 7
+REMINDER_DAYS_AFTER = 7
+
+BACKUP_PATH = 'backup/Khatabook_backup.db'
+DB_NAME = 'Khatabook.db'
+
+FROM_EMAIL = 'noreply@Khatabook.com'
+
